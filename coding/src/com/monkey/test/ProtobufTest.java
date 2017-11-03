e732f1cf8c567b9223fbe3b69ebc280bae8be0e1
beyondscl <727030664@qq.com>
2017-11-03 09:19:11 +0800

    add meaven & protobuf & netty

---
+++ coding/src/com/monkey/test/ProtobufTest.java
@@ -0,0 +1,13 @@
+package com.monkey.test;
+
+/**
+ * author: 牛虻.
+ * time:2017/11/2
+ * email:pettygadfly@gmail.com
+ * doc:
+ */
+public class ProtobufTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ coding/src/com/monkey/test/TestPerson.java
@@ -0,0 +1,14 @@
+package com.monkey.test;
+
+import com.monkey.outjava.Person;
+
+/**
+ * author: 牛虻.
+ * time:2017/11/2
+ * email:pettygadfly@gmail.com
+ * doc:
+ */
+public class TestPerson {
+    public static void main(String[] args) {
+    }
+}
