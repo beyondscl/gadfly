0e46c373bcd0b94f9ed1ffdd0e6465b3772f97ea
gadfly <pettygadfly@gmail.com>
2017-11-19 11:28:41 +0800

    添加接口说明

---
+++ coding/src/com/cat/TestSkill/TestInterface.java
@@ -0,0 +1,16 @@
+package com.cat.TestSkill;
+
+/**
+ * author: 牛虻.
+ * time:2017/11/19 0019
+ * email:pettygadfly@gmail.com
+ * doc:
+ */
+public interface TestInterface {
+    //变量都是public final static
+    String name = "myname";
+
+    //方法都是public 且不能加final ,static
+    void syaHello();
+
+}
