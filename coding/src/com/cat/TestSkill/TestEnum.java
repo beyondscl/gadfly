c6f8d7f798533ae149e530fb719b5de8ee039da5
gadfly <pettygadfly@gmail.com>
2017-11-27 22:52:20 +0800

    枚举类就是有包含有固定数量实例： 1.私有构造器不能被继承，构造器中不能使用static的东西 2.变量必须第一行声明 3.是final类，继承自java.lang.Enum

--- coding/src/com/cat/TestSkill/TestEnum.java
+++ coding/src/com/cat/TestSkill/TestEnum.java
@@ -6,12 +6,13 @@ package com.cat.TestSkill;
  * email:pettygadfly@gmail.com
  * doc:
  */
-public enum TestEnum {
-    INSTANCE;
+public enum TestEnum {//final
+    INSTANCE, INST2;//必须第一行声明
     private Single single;
+    private static int age;
 
-    TestEnum() {//默认私有
-        single = new Single();
+    TestEnum() {//私有，那么不能被继承
+        single = new Single();//不能用static
     }
 
     public Single getInstance() { //方法不要也可以
@@ -19,12 +20,25 @@ public enum TestEnum {
     }
 
     public static void main(String[] args) {
+        System.out.println(TestEnum.INSTANCE.toString());
         System.out.println(TestEnum.INSTANCE.single);
         System.out.println(TestEnum.INSTANCE.getInstance());
         System.out.println(TestEnum.INSTANCE.single == TestEnum.INSTANCE.getInstance());
+        for (TestEnum e : TestEnum.values()) {
+            System.out.println(e + " -->" + e.ordinal());
+        }
     }
 }
 
 class Single {
+    private static int age;
+
+    public Single() {
+        age = 2;
+    }
+}
+
+class Single2 extends Single {
+    private static int age;
 
 }
